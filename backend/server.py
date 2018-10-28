3b5cbcf30b62ba199f8f4872e4a26d79c48de478
Eri Bastos <bastos.eri@gmail.com>
2018-10-28 14:04:39 -0300

    add basic logging capabilities to the backend server

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,12 @@
 from concurrent import futures
+import logging
 import os
 import sys
 import time
-import django
 import pytz
 from datetime import datetime
 
+import django
 import grpc
 
 import history_pb2
@@ -15,13 +16,14 @@ import history_pb2_grpc
 from django.db import models
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Historian(history_pb2_grpc.HistorianServicer):
 
     def GetCommand(self, request):
 
-        #timestamp = datetime.utcfromtimestamp(request.timestamp)
         timestamp = datetime.fromtimestamp(request.timestamp, pytz.utc)
         command = ' '.join(request.command)
         new_command = Command(hostname=request.hostname, timestamp=timestamp,
@@ -34,6 +36,7 @@ class Historian(history_pb2_grpc.HistorianServicer):
 
 
 def serve(port):
+    logger.info('Starting shell-history backend service')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     try:
         with open('certs/localhost.key') as f:
@@ -43,11 +46,14 @@ def serve(port):
         server_credentials = grpc.ssl_server_credentials(
         server.add_secure_port('[::]:{}'.format(port), server_credentials)
         ((str.encode(private_key), str.encode(certificate_chain),),))
+        logger.info('Listening on secure port [::]:{}'.format(port))
     except:
+        logger.info('Listening on insecure port [::]:{}'.format(port))
         server.add_insecure_port('[::]:{}'.format(port))
 
     history_pb2_grpc.add_HistorianServicer_to_server(Historian(), server)
 
+    logger.info('Ready')
     server.start()
     try:
         while True:
